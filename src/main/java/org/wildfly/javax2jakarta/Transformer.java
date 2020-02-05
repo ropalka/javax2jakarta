6837b122ff4fe308c38c8f4fc43eeb8f7d38c193
Richard Opálka <opalka.richard@gmail.com>
2020-02-05 23:54:50 +0100

    First sketch

--- src/main/java/org/wildfly/javax2jakarta/Transformer.java
+++ src/main/java/org/wildfly/javax2jakarta/Transformer.java
@@ -21,9 +21,55 @@
  */
 package org.wildfly.javax2jakarta;
 
+import java.nio.ByteBuffer;
+
 /**
  * <a href="mailto:ropalka@redhat.com">Richard Opálka</a>
  */
 public final class Transformer {
-    // TODO: implement
+
+    private static final byte UTF8 = 1;
+    private static final byte INTEGER = 3;
+    private static final byte FLOAT = 4;
+    private static final byte LONG = 5;
+    private static final byte DOUBLE = 6;
+    private static final byte CLASS = 7;
+    private static final byte STRING = 8;
+    private static final byte FIELD_REF = 9;
+    private static final byte METHOD_REF = 10;
+    private static final byte INTERFACE_METHOD_REF = 11;
+    private static final byte NAME_AND_TYPE = 12;
+    private static final byte METHOD_HANDLE = 15;
+    private static final byte METHOD_TYPE = 16;
+    private static final byte DYNAMIC = 17;
+    private static final byte INVOKE_DYNAMIC = 18;
+    private static final byte MODULE = 19;
+    private static final byte PACKAGE = 20;
+
+    public byte[] transform(final byte[] clazz) {
+        final ByteBuffer classBB = ByteBuffer.wrap(clazz);
+        classBB.position(8);
+        final int poolSize = classBB.getShort();
+        byte tag;
+        for (int i = 1; i < poolSize; i++) {
+            tag = classBB.get();
+            if (tag == UTF8) {
+                // TODO: implement javax -> jakarta transformation
+            } else if (tag == CLASS || tag == STRING || tag == METHOD_TYPE || tag == MODULE || tag == PACKAGE) {
+                classBB.position(classBB.position() + 2);
+            } else if (tag == LONG || tag == DOUBLE) {
+                classBB.position(classBB.position() + 8);
+                i++; // see JVM specification
+            } else if (tag == INTEGER || tag == FLOAT || tag == FIELD_REF || tag == METHOD_REF
+            || tag == INTERFACE_METHOD_REF || tag == NAME_AND_TYPE
+            || tag == DYNAMIC || tag == INVOKE_DYNAMIC) {
+                classBB.position(classBB.position() + 4);
+            } else if (tag == METHOD_HANDLE) {
+                classBB.position(classBB.position() + 3);
+            } else {
+                throw new UnsupportedClassVersionError();
+            }
+        }
+        throw new UnsupportedOperationException(); // TODO: implement
+    }
 }
