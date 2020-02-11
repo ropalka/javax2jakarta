edfc589ecabf48f924e5c23171e404876006055b
Richard Opálka <opalka.richard@gmail.com>
2020-02-11 14:17:05 +0100

    Fixing UTF-8 to string decoding bug

--- src/main/java/org/wildfly/javax2jakarta/ClassFileUtils.java
+++ src/main/java/org/wildfly/javax2jakarta/ClassFileUtils.java
@@ -78,9 +78,9 @@ final class ClassFileUtils {
             if ((currentByte & 0x80) == 0) {
                 charBuffer[charsCount++] = (char) (currentByte & 0x7F);
             } else if ((currentByte & 0xE0) == 0xC0) {
-                charBuffer[charsCount++] = (char) (((currentByte & 0x1F) << 6) + (clazz[offset + processedBytes++] & 0x3F));
+                charBuffer[charsCount++] = (char) (((currentByte & 0x1F) << 6) + (clazz[processedBytes++] & 0x3F));
             } else {
-                charBuffer[charsCount++] = (char) (((currentByte & 0xF) << 12) + ((clazz[offset + processedBytes++] & 0x3F) << 6) + (clazz[offset + processedBytes++] & 0x3F));
+                charBuffer[charsCount++] = (char) (((currentByte & 0xF) << 12) + ((clazz[processedBytes++] & 0x3F) << 6) + (clazz[processedBytes++] & 0x3F));
             }
         }
         return new String(charBuffer, 0, charsCount);
